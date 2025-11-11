e586e610617cdf07281ccd3fc8bc3ce71f3d83c2
Rcong <zcr0903@gmail.com>
2025-11-11 15:30:36 +0800

    init

---
+++ QuanterDefi-fe/src/pages/strategies.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Strategies: NextPage = () => {
+  return (
+    <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <h1 className="text-4xl font-bold text-gray-900">Strategies</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Strategies;
